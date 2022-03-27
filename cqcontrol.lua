91996ca28668c91b39fe40b85d6020a9d9660155
Apethesis <86126617+Apethesis@users.noreply.github.com>
2022-03-28 00:20:50 +0200

    Update cqcontrol.lua

--- cqcontrol.lua
+++ cqcontrol.lua
@@ -1,4 +1,4 @@
-local ver = 1.1
+local ver = 1.2
 local request = http.get("https://raw.githubusercontent.com/Apethesis/CC-Code/main/cqcontrol.lua")
 local version = request.readLine()
 request.close()
@@ -10,29 +10,18 @@ if not (ver == verNum) then
     newver.write(request.readAll())
     request.close()
     newver.close()
-    print("Updated")
-    os.reboot()
+    error("Updated",0)
 end
 local modem = peripheral.find("modem") or error("No modem attached","0")
-local modtable = {
-    [turtle.forward] = "w",
-    [turtle.turnLeft] = "q",
-    [turtle.turnRight] = "e",
-    [turtle.refuel] = "r",
-    [turtle.back] = "s",
-    [turtle.dig] = "z",
-    [turtle.digUp] = "x",
-    [turtle.digDown] = "c"
-}
 local keytable = {
-    [keys.w] = turtle.forward,
-    [keys.q] = turtle.turnLeft,
-    [keys.e] = turtle.turnRight,
-    [keys.r] = turtle.refuel,
-    [keys.s] = turtle.back,
-    [keys.z] = turtle.dig,
-    [keys.x] = turtle.digUp,
-    [keys.c] = turtle.digDown
+    [keys.w] = "w",
+    [keys.q] = "q",
+    [keys.e] = "e",
+    [keys.r] = "r",
+    [keys.s] = "s",
+    [keys.z] = "z",
+    [keys.x] = "x",
+    [keys.c] = "c"
 }
 if modem.isOpen(4221) == false then
     modem.open(4221)
@@ -48,6 +37,6 @@ print("X = Dig Up")
 print("C = Dig Down")
 while true do
     local event, key, is_held = os.pullEvent("key")
-    modem.transmit(4221,4221,modtable[keytable[key]])
+    modem.transmit(4221,4221,keytable[key])
     sleep()
 end
