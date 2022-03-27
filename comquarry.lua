835d49bdf6905b70b4199121b93467e37fdf69f7
Apethesis <86126617+Apethesis@users.noreply.github.com>
2022-03-27 23:16:13 +0200

    Update comquarry.lua

--- comquarry.lua
+++ comquarry.lua
@@ -1,12 +1,12 @@
-local ver = 1.1
+local ver = 1.2
 local request = http.get("https://raw.githubusercontent.com/Apethesis/CC-Code/main/comquarry.lua")
 local version = request.readLine()
 request.close()
 local verNum = tonumber(version:match("= (.+)"))
 if not (ver == verNum) then
-    fs.delete("./startup.lua")
+    fs.delete(shell.getRunningProgram())
     local request = http.get("https://raw.githubusercontent.com/Apethesis/CC-Code/main/comquarry.lua")
-    local newver = fs.open("./startup.lua","w")
+    local newver = fs.open(shell.getRunningProgram(),"w")
     newver.write(request.readAll())
     request.close()
     newver.close()
@@ -15,14 +15,14 @@ if not (ver == verNum) then
 end
 local modem = peripheral.find("modem") or error("No modem attached","0")
 local keytable = {
-    ["w"] = turtle.forward(),
-    ["q"] = turtle.turnLeft(),
-    ["e"] = turtle.turnRight(),
-    ["r"] = turtle.refuel(),
-    ["s"] = turtle.back(),
-    ["z"] = turtle.dig("left"),
-    ["x"] = turtle.digUp("left"),
-    ["c"] = turtle.digDown("left")
+    ["w"] = turtle.forward,
+    ["q"] = turtle.turnLeft,
+    ["e"] = turtle.turnRight,
+    ["r"] = turtle.refuel,
+    ["s"] = turtle.back,
+    ["z"] = turtle.dig,
+    ["x"] = turtle.digUp,
+    ["c"] = turtle.digDown
 }
 if modem.isOpen(4221) == false then
     modem.open(4221)
