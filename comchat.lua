2a59c1a01b8229d8afc72774cebce0b91f1d1c64
Apethesis <86126617+Apethesis@users.noreply.github.com>
2022-03-27 07:38:50 +0200

    Create comchat.lua

---
+++ comchat.lua
@@ -0,0 +1,73 @@
+local ver = 1.1
+local request = http.get("https://raw.githubusercontent.com/Apethesis/CC-Code/main/comchat.lua")
+local version = request.readLine()
+request.close()
+local verNum = tonumber(version:match("= (.+)"))
+if not (ver == verNum) then
+    fs.delete("./comchat.lua")
+    fs.delete("./comlib.lua")
+    local request = http.get("https://raw.githubusercontent.com/Apethesis/CC-Code/main/comchat.lua")
+    local newver = fs.open("./compaint.lua","w")
+    newver.write(request.readAll())
+    request.close()
+    newver.close()
+    error("ComPaint updated.",0)
+end
+local modem = peripheral.find("modem") or error("No modem attached","0")
+if fs.exists("./comlib.lua") == false then
+    local htg = http.get("https://raw.githubusercontent.com/Apethesis/CC-Code/main/comlib.lua")
+    local htf = fs.open("./comlib.lua","w")
+    htf.write(htg.readAll())
+    htf.close()
+    htg.close()
+end
+if fs.exists("./sha256.lua") == false then
+    local htg = http.get("https://pastebin.com/raw/6UV4qfNF")
+    local htf = fs.open("./sha256.lua","w")
+    htf.write(htg.readAll())
+    htf.close()
+    htg.close()
+end
+local comlib = require("comlib")
+if modem.isOpen(4557) == false then
+    modem.open(4557)
+    modem.transmit(4557,4557,"Channel open.")
+end
+local ttable = {}
+local rtable = {}
+if fs.exists("/.exodus/lesserchat/settings") == false then
+    local sfile = fs.open("/.exodus/lesserchat/settings","w")
+    ttable["name"] = nil
+    ttable["message"] = nil
+    print("Enter a username:")
+    local nm = read()
+    ttable["name"] = nm
+    sfile.write(textutils.serialize(ttable))
+    sfile.close()
+else
+    local sfile = fs.open("/.exodus/lesserchat/settings","r")
+    ttable = textutils.unserialize(sfile.readAll())
+end
+function send()
+    while true do
+        local msg = read()
+        ttable["message"] = msg
+        modem.transmit(4557,4557,ttable)
+        print(ttable["name"].." > "..msg)
+        sleep()
+    end
+end
+function recieve()
+    while true do
+        local _,_,_,_,rmsg,_ = os.pullEvent("modem_message")
+        rtable = rmsg
+        if rtable["name"] ~= ttable["name"] and rtable["message"] ~= msg then
+            print(rtable["name"].." > "..rtable["message"])
+        end
+        sleep()
+    end
+end
+while true do
+    parallel.waitForAny(recieve,send)
+    sleep()
+end
