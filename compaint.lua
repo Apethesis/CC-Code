10f0b79eb6afe7345903ceefddabe0e501c51371
Apethesis <86126617+Apethesis@users.noreply.github.com>
2022-03-29 16:51:34 +0200

    Update compaint.lua

--- compaint.lua
+++ compaint.lua
@@ -1,4 +1,4 @@
-local ver = 3.6
+local ver = 3.7
 if not fs.exists("./comlib.lua") then
     local htg = http.get("https://raw.githubusercontent.com/Apethesis/CC-Code/main/comlib.lua")
     local htf = fs.open("./comlib.lua","w")
