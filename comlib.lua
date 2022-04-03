52990b6eed4736bb6e561e6cfec13b7189d20e58
manaphoenix <toehead976@gmail.com>
2022-04-03 14:54:42 -0400

    fixed comlib

--- comlib.lua
+++ comlib.lua
@@ -1,86 +1,59 @@
 local comlib = {}
-local expect = require("cc.expect")
+local color_hex_lookup = {}
 
 function comlib.toBlit(color)
-    local color_hex_lookup = {
-        [colors.white] = "0",
-        [colors.orange] = "1",
-        [colors.magenta] = "2",
-        [colors.lightBlue] = "3",
-        [colors.yellow] = "4",
-        [colors.lime] = "5",
-        [colors.pink] = "6",
-        [colors.gray] = "7",
-        [colors.lightGray] = "8",
-        [colors.cyan] = "9",
-        [colors.purple] = "a",
-        [colors.blue] = "b",
-        [colors.brown] = "c",
-        [colors.green] = "d",
-        [colors.red] = "e",
-        [colors.black] = "f"
-    }
-    expect(1, color, "number")
-    return color_hex_lookup[color] or
-        string.format("%x", math.floor(math.log(color) / math.log(2)))
+    if color == nil or type(color) ~= "number" then
+        return nil
+    end
+    return color_hex_lookup[color] or string.format("%x", math.floor(math.log(color) / math.log(2)))
 end
 
-function comlib.prite(x,y,text,tcolor,bcolor)
-    expect(1, x, "number")
-    expect(2, y, "number")
-    expect(3, text, "string")
-    expect(4, tcolor, "string", "number", nil)
-    expect(5, bcolor, "string", "number", nil)
-    if tcolor == nil then
-        tcolor = "0"
+for i,v in pairs(colors) do
+    if type(v) == "number" then
+        color_hex_lookup[i] = comlib.toBlit(v)
     end
-    if bcolor == nil then
-        bcolor = "f"
-    end
-    if type(tcolor) == "number" then
-        tcolor = comlib.toBlit(tcolor)
-    end
-    if type(bcolor) == "number" then
-        bcolor = comlib.toBlit(bcolor)
-    end
-    term.setCursorPos(x,y)
-    term.blit(text,tcolor:rep(#text),bcolor:rep(#text))
+end
+
+function comlib.prite(x, y, text, tcolor, bcolor)
+    x = x or 0
+    y = y or 0
+    text = text or ""
+    tcolor = tcolor or "0"
+    bcolor = bcolor or "f"
+    tcolor = type(tcolor) == "number" and comlib.toBlit(tcolor) or tcolor
+    bcolor = type(bcolor) == "number" and comlib.toBlit(bcolor) or bcolor
+    term.setCursorPos(x, y)
+    term.blit(text, tcolor:rep(#text), bcolor:rep(#text))
 end -- this is prite
 
 function comlib.encode(tbl)
-    local out = tostring(#tbl[#tbl]).."|"
-    for k,v in ipairs(tbl) do
-        out = out .. v or ""
-    end
+    local out = tostring(#tbl[#tbl]) .. "|"
+    for k, v in ipairs(tbl) do out = (out .. v) or "" end
     return out
 end
 
 function comlib.decode(str)
-    local len_or,str = str:match("(%d.-)|(.+)")
-    local len = #str/len_or
+    local len_or, str = str:match("(%d.-)|(.+)")
+    local len = #str / len_or
     local out = {}
-    for i=1,len do
-        out[i] = str:sub(i*len_or-len_or+1,i*len_or)
-    end
+    for i = 1, len do out[i] = str:sub(i * len_or - len_or + 1, i * len_or) end
     return out
 end
 
-function comlib.update(link,ver)
+function comlib.update(link, ver)
     local request = http.get(link)
     if request ~= nil then
-        local version = request.readLine()
-        local verNum = tonumber(version:match("= (.+)"))
+        local txt = request.readAll()
+        local verNum = tonumber(txt:match("local ver = (.+)"))
+        request.close()
         if ver < verNum then
-            local newver = fs.open(shell.getRunningProgram(),"w")
-            newver.write(request.readAll())
-            request.close()
+            local newver = fs.open(shell.getRunningProgram(), "w")
+            newver.write(txt)
             newver.close()
             return true
         end
-    elseif request == nil then
-        return false
     end
-    request.close()
+    return false
 end
 --[[
     to use comlib.update you need to have a direct link to the latest version
