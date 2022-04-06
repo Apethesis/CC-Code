a4181f2f2eb9375ccd49de9649dd919641c98ff7
Apethesis <86126617+Apethesis@users.noreply.github.com>
2022-04-06 15:18:14 +0200

    Update pecquarry.lua

--- pecquarry.lua
+++ pecquarry.lua
@@ -1,17 +1,15 @@
 local ver = 1.2
 local request = http.get("https://raw.githubusercontent.com/Apethesis/CC-Code/main/pecquarry.lua")
-local version = request.readLine()
-request.close()
-local verNum = tonumber(version:match("= (.+)"))
-if not (ver == verNum) then
-    fs.delete(shell.getRunningProgram())
-    local request = http.get("https://raw.githubusercontent.com/Apethesis/CC-Code/main/pecquarry.lua")
-    local newver = fs.open(shell.getRunningProgram(),"w")
-    newver.write(request.readAll())
-    request.close()
-    newver.close()
-    print("Updated")
-    os.reboot()
+if not fs.exists("./peclib.lua") then
+    local htg = http.get("https://raw.githubusercontent.com/Apethesis/CC-Code/main/peclib.lua")
+    local htf = fs.open("./peclib.lua","w")
+    htf.write(htg.readAll())
+    htf.close()
+    htg.close()
+end
+local peclib = require "peclib"
+if peclib.update("https://raw.githubusercontent.com/Apethesis/CC-Code/main/pecchat.lua",ver) then
+	error("PecQuarry updated.",0)
 end
 local modem = peripheral.find("modem") or error("No modem attached","0")
 local keytable = {
