971f6c20d585d8f0561f2636214adac9c0c3ca38
Apethesis <86126617+Apethesis@users.noreply.github.com>
2022-05-10 22:45:55 +0200

    Update combtAPI.lua

--- combtAPI.lua
+++ combtAPI.lua
@@ -19,15 +19,23 @@ local color_hex_lookup = {
     [colors.black] = "f"
 }
 
+---@param x number x coordinate of the click
+---@param y number y coordinate of the click
+---@param start_x number start x
+---@param start_y number start y
+---@param width number width
+---@param height number height
 function combtAPI.mbIsWithin(x,y,start_x,start_y,width,height)
     return x >= start_x and x < start_x+width and y >= start_y and y < start_y+height
 end
 
+---@description You are not going to use this, so I wont even docstring it.
 function combtAPI.writeCentered(x,y,w,h,text,bg,fg)
     term.setCursorPos(math.ceil(w/2-#text/2+x),math.ceil(h/2)+y-1)
     term.blit(text,fg:rep(#text),bg:rep(#text))
 end
 
+---@param maintbl table the main table
 function combtAPI.makeButton(maintbl)
     expect(1, maintbl, "table")
     expect(2, maintbl.x, "number")
@@ -37,6 +45,7 @@ function combtAPI.makeButton(maintbl)
     expect(6, maintbl.text, "string", "nil")
     expect(7, maintbl.textColor, "string", "number", "nil")
     expect(8, maintbl.color, "string", "number", "nil")
+    expect(8, maintbl.visible, "boolean", "nil")
     expect(9, maintbl.buttonName, "string")
     combtTBL = combtTBL or {}
     local textColorC = maintbl.textColor
@@ -57,10 +66,12 @@ function combtAPI.makeButton(maintbl)
         height = maintbl.height,
         text = maintbl.text,
         textColor = maintbl.textColor,
-        color = maintbl.color
+        color = maintbl.color,
+        visible = maintbl.visible
     }
 end
 
+---@param buttonName string the button name
 function combtAPI.drawButton(buttonName)
     expect(1, buttonName, "string")
     if combtTBL[buttonName] == nil then 
@@ -77,14 +88,21 @@ function combtAPI.drawButton(buttonName)
     if type(button.color) == "number" then
         button.color = color_hex_lookup[button.color]
     end
+    if button.visible == false then
+        textColorC = color_hex_lookup[term.getTextColor()]
+        colorC = color_hex_lookup[term.getBackgroundColor()]
+    end
     button.text = button.text or ""
     for i=button.y,button.y+button.height-1 do
         term.setCursorPos(button.x,i)
-        term.blit(string.rep(" ",button.width),button.textColor:rep(button.width),button.color:rep(button.width))
+        term.blit(string.rep(" ",button.width),textColorC:rep(button.width),colorC:rep(button.width))
     end
-    combtAPI.writeCentered(button.x,button.y,button.width,button.height,button.text,colorC,textColorC)
+    if button.visible == true then
+        combtAPI.writeCentered(button.x,button.y,button.width,button.height,button.text,colorC,textColorC)
+    end
 end
 
+---@param maintbl table
 function combtAPI.modifyButton(maintbl)
     expect(1, maintbl, "table")
     expect(2, maintbl.x, "number", "nil")
@@ -94,7 +112,8 @@ function combtAPI.modifyButton(maintbl)
     expect(6, maintbl.text, "string", "nil")
     expect(7, maintbl.textColor, "string", "number", "nil")
     expect(8, maintbl.color, "string", "number", "nil")
-    expect(9, maintbl.buttonName, "string")
+    expect(9, maintbl.visible, "boolean", "nil")
+    expect(10, maintbl.buttonName, "string")
     combtTBL = combtTBL or {}
     if combtTBL[maintbl.buttonName] == nil then 
         error("Button not found")
@@ -106,6 +125,7 @@ function combtAPI.modifyButton(maintbl)
     maintbl.text = maintbl.text or combtTBL[maintbl.buttonName].text
     maintbl.textColor = maintbl.textColor or combtTBL[maintbl.buttonName].textColor
     maintbl.color = maintbl.color or combtTBL[maintbl.buttonName].color
+    maintbl.visible = maintbl.visible or combtTBL[maintbl.buttonName].visible
     if type(maintbl.textColor) == "number" then
         maintbl.textColor = color_hex_lookup[maintbl.textColor]
     end
@@ -119,19 +139,31 @@ function combtAPI.modifyButton(maintbl)
     combtTBL[maintbl.buttonName].text = maintbl.text
     combtTBL[maintbl.buttonName].textColor = maintbl.textColor
     combtTBL[maintbl.buttonName].color = maintbl.color
+    combtTBL[maintbl.buttonName].visible = maintbl.visible
     combtAPI.drawButton(maintbl.buttonName)
 end
 
+---@param buttonName string
+---@param mx number
+---@param my number
 function combtAPI.check(buttonName,mx,my)
     expect(1,buttonName,"string")
     expect(2,mx,"number")
     expect(3,my,"number")
     local maintbl = combtTBL[buttonName]
+    if maintbl.visible == false then
+        return false
+    end
     if combtAPI.mbIsWithin(mx, my, maintbl.x, maintbl.y, maintbl.width, maintbl.height) then
         return true
     else
         return false
     end
+end
+
+function combtAPI.removeButton(buttonName)
+    expect(1,buttonName,"string")
+    combtTBL[buttonName] = nil
 end
 
 return combtAPI
