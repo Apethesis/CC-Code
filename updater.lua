35abf99fbfefbb293b24ff3742c4bc91746b0e65
TotallyNotVirtIO <tobich228@gmail.com>
2022-05-28 19:58:01 +0000

    Removing peclib, part one.

---
+++ updater.lua
@@ -0,0 +1,14 @@
+
+local function update(link, ver)
+    --[[local req, err = http.request(link)
+    assert(req, err)
+
+    local txt = req.readAll()
+    local GHVer = tonumber(txt:match("%d%.%d$"))
+    if GHVer > ver then
+        local f = fs.open(shell.getRunningProgram(), "w")
+        f.write(txt)
+    end--]]
+end
+
+return update
