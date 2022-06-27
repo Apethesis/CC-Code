e46c7457a7169ad5b34286626bd54f16137f387b
Apethesis <86126617+Apethesis@users.noreply.github.com>
2022-06-27 23:11:49 +0200

    Add files via upload

---
+++ combtAPIexample.lua
@@ -0,0 +1,17 @@
+local api = require("combtAPI2")
+local canvas = api.create()
+canvas:make({
+    x=2,y=2,width=4,height=3,
+    color=colors.green,textColor=colors.red,
+    text="hi", name="hiButton", on_click=function()
+        error("it works",0)
+    end
+})
+canvas:draw("hiButton")
+local function hello()
+    while true do
+        print("hello worldie")
+        sleep(3)
+    end
+end
+canvas:execute(hello)
