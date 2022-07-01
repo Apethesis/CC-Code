8460214435401a9f795d27faf1be1eb37f26045f
Apethesis <apethesis@gmail.com>
2022-07-01 23:02:55 +0200

    moved from if statements to assert

--- ccpeg/ccpeg.lua
+++ ccpeg/ccpeg.lua
@@ -5,13 +5,8 @@ if args[1] == nil then
 else
     local filein = fs.open(args[1], "r")
     local fileout = fs.open(args[2], "w")
-    local type1 = require("decode."..args[1]:match("[^%.]+$"))
-    local type2 = require("encode."..args[2]:match("[^%.]+$"))
-    if type1 == nil then
-        error("Unable to decode format, reason: Format Not Supported",0)
-    elseif type2 == nil then
-        error("Unable to encode format, reason: Format Not Supported",0)
-    end
+    local type1 = assert(require("decode."..args[1]:match("[^%.]+$")), "input format not supported")
+    local type2 = assert(require("encode."..args[2]:match("[^%.]+$")), "output format not supported")
     local tbl = type1(filein)
     type2(tbl, fileout)
 end
