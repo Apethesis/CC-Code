129706fd290848f17fb519567a2daeed542f4faa
Apethesis <apethesis@gmail.com>
2022-07-14 18:05:19 +0200

    made combtAPI2 use window

--- combtAPI2.lua
+++ combtAPI2.lua
@@ -6,15 +6,16 @@ local function to_blit(c)
 	return ("%x"):format(select(2, math.frexp(c))-1)
 end
 
-local function prite(x, y, text, tcolor, bcolor)
-    x = x or 0 y = y or 0 text = text or "" tcolor = tcolor or "0" bcolor = bcolor or "f" tcolor = type(tcolor) == "number" and to_blit(tcolor) or tcolor bcolor = type(bcolor) == "number" and to_blit(bcolor) or bcolor
-    term.setCursorPos(x, y)
-    term.blit(text, tcolor:rep(#text), bcolor:rep(#text))
+local function prite(x, y, text, tcolor, bcolor, trm)
+    trm = trm or term.current() x = x or 0 y = y or 0 text = text or "" tcolor = tcolor or "0" bcolor = bcolor or "f" tcolor = type(tcolor) == "number" and to_blit(tcolor) or tcolor bcolor = type(bcolor) == "number" and to_blit(bcolor) or bcolor
+    trm.setCursorPos(x, y)
+    trm.blit(text, tcolor:rep(#text), bcolor:rep(#text))
 end
 
-local function writeCentered(x,y,w,h,text,bg,fg)
-    term.setCursorPos(math.ceil(w/2-#text/2+x),math.ceil(h/2)+y-1)
-    term.blit(text,fg:rep(#text),bg:rep(#text))
+local function writeCentered(x,y,w,h,text,bg,fg,trm)
+    trm = trm or term.current()
+    trm.setCursorPos(math.ceil(w/2-#text/2+x),math.ceil(h/2)+y-1)
+    trm.blit(text,fg:rep(#text),bg:rep(#text))
 end
 
 local function mbIsWithin(x,y,start_x,start_y,width,height)
@@ -22,9 +23,11 @@ local function mbIsWithin(x,y,start_x,start_y,width,height)
 end
 
 -- The start of all, api.create()
-function api.create(terme)
-    functable.term = terme or term.current()
-    return setmetatable({ data = {}, tasks = {} },{__index = functable, __add = function(self,other)
+function api.create(terme,x,y,width,height)
+    terme = terme or term.current()
+    x = x or 1 y = y or 1 width, height = term.getSize() 
+    terme = window.create(terme,x,y,width,height)
+    return setmetatable({ data = {}, tasks = {}, term = terme },{__index = functable, __add = function(self,other)
         if type(other) == "table" then
             for k,v in pairs(other) do
                 table.insert(self,#self+1,v)
@@ -62,13 +65,13 @@ function functable:draw(name)
     local tbl = self.data[name]
     if not tbl.visible then return end
     for i=tbl.y,tbl.y+tbl.height-1 do
-        prite(tbl.x,i,(" "):rep(tbl.width),tbl.textColor,tbl.color)
+        prite(tbl.x,i,(" "):rep(tbl.width),tbl.textColor,tbl.color,self.term)
     end
-    writeCentered(tbl.x,tbl.y,tbl.width,tbl.height,tbl.text,tbl.color,tbl.textColor)
+    writeCentered(tbl.x,tbl.y,tbl.width,tbl.height,tbl.text,tbl.color,tbl.textColor,self.term)
 end
 
 function functable:schedule(func)
-    self.tasks = self.tasks + {coro = coroutine.new(func)}
+    table.insert(self.tasks,#self.tasks+1,{coro = coroutine.create(func)})
 end
 
 function functable:execute(func)
--- combtAPIexample.lua
+++ combtAPIexample.lua
@@ -4,15 +4,20 @@ canvas:make({
     x=2,y=2,width=4,height=3,
     color=colors.green,textColor=colors.red,
     text="hi", name="hiButton", on_click=function()
-        error("it works",0)
+        print("hiButton1")
+        sleep(3)
+        print("hiButton2")
     end
 })
 canvas:draw("hiButton")
-local function hello()
-    while true do
-        print("hello worldie")
-        canvas:draw("hiButton")
-        sleep(3)
+canvas:make({
+    x=6,y=2,width=5,height=3,
+    color=colors.green,textColor=colors.red,
+    text="hoi", name="hoiButton", on_click=function()
+        print("hoiButton1")
+        sleep(5)
+        print("hoiButton2")
     end
-end
-canvas:execute(hello)
+})
+canvas:draw("hoiButton")
+canvas:execute()
