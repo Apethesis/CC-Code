edec7d5e432a363b49634e085d110bc3c751da3b
Apethesis <apethesis@gmail.com>
2022-09-27 01:52:23 +0200

    Add files via upload

---
+++ l4ltst.lua
@@ -0,0 +1,7 @@
+local log = require("log4l")
+local curlog = log.create()
+curlog:log("Log4L started, welcome :)","test")
+curlog:log("This is a warn","test",1)
+curlog:log("This is an error","test",2)
+curlog:log("And this... is true power (jk)","test",3)
+curlog:stop()
